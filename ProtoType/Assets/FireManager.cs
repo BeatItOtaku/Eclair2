d3d168736650aa3e084a2b7b66964bea01aab2e3
naotee <naotee917@gmail.com>
2016-12-22 12:27:09 +0900

    FireManagerを修正した アニメーション内にスクリプトを入れた

--- ProtoType/Assets/FireManager.cs
+++ ProtoType/Assets/FireManager.cs
@@ -6,8 +6,7 @@ public class FireManager : MonoBehaviour {
 	public static bool isShot = true; //falseでエクレアは射撃ができなくなる。
 	public static bool isAttack = true; //falseでエクレアは近接攻撃ができなくなる。
 
-
-	private bool fire = false;
+	private bool fire = false; //攻撃を繰り出したかどうか
 
 	private int fireCount = 0; //攻撃した回数
 
@@ -22,6 +21,9 @@ public class FireManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		Debug.Log ("Fire");
+		Debug.Log ("fireCount");
+
 	}
 	//Fire
 	public void FireManagement(){
@@ -29,25 +31,29 @@ public class FireManager : MonoBehaviour {
 			switch (fireCount) {
 
 			case 1:
-				anim.SetTrigger ("Fire1");
+				anim.SetBool ("Fire1",true);
 				break;
 			case 2:
-				anim.SetTrigger ("Fire2");
+				anim.SetBool ("Fire2",true);
 				break;
 			case 3:
-				anim.SetTrigger ("Fire3");
+				anim.SetBool ("Fire3",true);
 				break;
 			case 4:
-				anim.SetTrigger ("Fire4");
+				anim.SetBool ("Fire4",true);
 				break;
 		}
 	}
 
 	IEnumerator fireCountCoroutine(){
 		if (Input.GetButtonDown ("Fire")) {
+			if (fireCount <= 4)
+			{
+				fireCount++;
+			}
 			if(!fire)
-				anim.SetTrigger ("Fire");
-			fireCount++;
+				anim.SetBool ("Fire",true);
+			
 			fire = true;
 			yield return new WaitForSeconds (1);
 			fire = false;
