7b4352d43cda690ffd495251ca065a11d135200f
naotee <naotee917@gmail.com>
2017-02-08 23:47:10 +0900

    照準調査

--- ProtoType/Assets/PointerManagement.cs
+++ ProtoType/Assets/PointerManagement.cs
@@ -8,31 +8,137 @@ public class PointerManagement : MonoBehaviour {
 	public static bool pointOnEdge = true;
 	private bool edge = false;
 
-	private bool upEdge = false;
-	private bool downEdge = false;
-	private bool leftEdge = false;
-	private bool rightEdge = false;
+	private bool isTopEdge = false;
+	private bool isBottomEdge = false;
+	private bool isLeftEdge = false;
+	private bool isRightEdge = false;
+
+	private bool sideEdge = false;
+	private bool heightEdge = false;
+
+	private bool outEdge = false;
 
 	public RectTransform cursor;
 	private Vector3 coursorPosition;
+
+	private Vector3 leftTopPosition;
+	private Vector3 leftBottomPosition;
+	private Vector3 rightTopPosition;
+	private Vector3 rightBottomPosition;
+
+	private float leftEdge;
+	private float rightEdge;
+	private float topEdge;
+	private float bottomEdge;
+
 	private float edgePositionX;
 	private float edgePositionY;
 	//public Texture2D cursorTexture;
 	//private CursorMode cursorMode = CursorMode.ForceSoftware;
 	//private Vector2 hotSpot;
 
+
 	// Use this for initialization
 	void Start () {
-		//hotSpot = new Vector2 (0, 0);
-	
+		mousePosition = Input.mousePosition;
+		leftEdge = Screen.width / 10;
+		rightEdge = Screen.width * 9 / 10;
+		topEdge = Screen.height / 10;
+		bottomEdge = Screen.height * 9 / 10;
 	}
 
 	// Update is called once per frame
-	void Update () {
-		mousePosition = Input.mousePosition;
-		//Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+	void Update ()
+	{
+		
 
-		if (mousePosition.x <= Screen.width / 10) {
+		leftTopPosition = new Vector3 (leftEdge, topEdge, mousePosition.z);
+		leftBottomPosition = new Vector3 (leftEdge, bottomEdge, mousePosition.z);
+		rightTopPosition = new Vector3 (rightEdge, topEdge, mousePosition.z);
+		rightBottomPosition = new Vector3 (rightEdge, bottomEdge, mousePosition.z);
+
+
+		//マウスの位置が左の辺を超えたとき
+		if (mousePosition.x <= leftEdge) {
+			isLeftEdge = true;
+		} else {
+			isLeftEdge = false;
+		}
+
+		//マウスの位置が右の辺を超えたとき
+		if (mousePosition.x >= rightEdge) {
+			isRightEdge = true;
+		} else {
+			isRightEdge = false;
+		}
+	
+		//マウスの位置が下の辺を超えたとき
+		if (mousePosition.y <= bottomEdge) {
+			isBottomEdge = true;
+		} else {
+			isBottomEdge = false;
+		}
+
+		//マウスの位置が上の辺を超えたとき
+		if (mousePosition.y >= topEdge) {
+			isTopEdge = true;
+		} else {
+			isTopEdge = false;
+		}
+			
+		//マウスの位置が上の辺と左の辺を超えたとき
+		if (isTopEdge && isLeftEdge)
+			cursor.position = leftTopPosition;
+		outEdge = true;
+
+		//マウスの位置が上の辺と右の辺を超えたとき
+		if (isTopEdge && isRightEdge)
+			cursor.position = rightTopPosition;
+		outEdge = true;
+
+		//マウスの位置が下の辺と左の辺を超えたとき
+		if (isBottomEdge && isLeftEdge)
+			cursor.position = leftBottomPosition;
+		outEdge = true;
+		//マウスの位置が下の辺と右の辺を超えたとき
+		if (isBottomEdge && isRightEdge)
+			cursor.position = rightBottomPosition;
+		outEdge = true;
+
+		if (!outEdge) {
+			if (isLeftEdge || isRightEdge) {
+				pointOnEdge = true;
+				heightEdge = false;
+				if (!sideEdge) {
+					edgePositionX = cursor.position.x;
+					sideEdge = true;
+				}
+				coursorPosition = new Vector3 (edgePositionX, mousePosition.y, mousePosition.z);
+				cursor.position = coursorPosition;
+			}
+			else if (isTopEdge || isBottomEdge) 
+			{
+				pointOnEdge = true;
+				sideEdge = false;
+				if (!heightEdge) {
+					edgePositionY = cursor.position.y;
+					heightEdge = true;
+				}
+				coursorPosition = new Vector3 (mousePosition.x, edgePositionY, mousePosition.z);
+				cursor.position = coursorPosition;
+			}
+		}
+		sideEdge = false;
+		heightEdge = false;
+		pointOnEdge = false;
+		outEdge = false;
+		cursor.position = Input.mousePosition;
+	}
+}
+
+
+		/*
+		 * if (mousePosition.x <= Screen.width / 10) {
 			pointOnEdge = true;
 			if (!leftEdge) {
 				edgePositionX = cursor.position.x;
@@ -81,4 +187,5 @@ public class PointerManagement : MonoBehaviour {
 			cursor.position = Input.mousePosition;
 		}
 	}
-}
+}*/
+
