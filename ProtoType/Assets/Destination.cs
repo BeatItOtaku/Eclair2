217f4a2a415e74d8d69c23f56a4213287f2938eb
wararyo <wararyo@gmail.com>
2017-02-27 18:11:03 +0900

    目的地表示
    まだECMが入ってないので目的地表示関係のCueEventは作ってません

---
+++ ProtoType/Assets/Destination.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Destination : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target) {
+			GetComponent<RectTransform> ().position = RectTransformUtility.WorldToScreenPoint (Camera.main, target.transform.position);
+			int distance = (int)(Vector3.Distance(player.transform.position,target.transform.position) + 0.5f);//四捨五入のための+0.5f
+			GetComponentInChildren<Text>().text = distance + "m";
+		}
+	}
+}
