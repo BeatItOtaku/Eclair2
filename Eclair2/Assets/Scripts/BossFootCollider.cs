9e6ae1772630d3b48553264700bbd83274f55432
naotee <naotee917@gmail.com>
2017-04-04 14:24:31 +0900

    ボス関係のファイルを入れた

---
+++ Eclair2/Assets/Scripts/BossFootCollider.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossFootCollider : MonoBehaviour {
+
+	public static bool bossFootAttack = false;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //footTime += Time.deltaTime;;
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+	private void OnTriggerEnter (Collider collider)
+	{
+        if (collider.gameObject.tag == "Player")
+        {
+
+            bossFootAttack = true;
+            player.GetComponent<PlayerControl>().Damage(10);
+        }
+			
+}
+}
