a89ed095b9b17c8c861f3beef172fe73bd153755
naotee <naotee917@gmail.com>
2017-04-07 21:38:20 +0900

    奈落追加中

---
+++ Eclair2/Assets/Naraku.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Naraku : MonoBehaviour {
+
+	public GameObject player;
+
+	public Transform restartPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag == "Player")
+		{
+			Anten.antenStart = true;
+
+		}
+
+
+	}
+}
