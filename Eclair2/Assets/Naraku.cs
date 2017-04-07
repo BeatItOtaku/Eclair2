10d4234fe78185eb4ee9f36f81dbac0b7dedd5f0
naotee <naotee917@gmail.com>
2017-04-07 22:50:14 +0900

    奈落の記述を追加

--- Eclair2/Assets/Naraku.cs
+++ Eclair2/Assets/Naraku.cs
@@ -1,20 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Naraku : MonoBehaviour {
 
 	public GameObject player;
+	private Vector3 playerPosition;
 
-	public Transform restartPosition;
+	private GameObject[] restartPosition;
+
+	private List<float> position = new List<float>();
 
 	// Use this for initialization
 	void Start () {
-	
+
+		restartPosition = GameObject.FindGameObjectsWithTag ("RestartPosition");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		playerPosition = player.transform.position;
+
 	}
 
 	private void OnTriggerEnter(Collider col){
@@ -22,7 +29,13 @@ public class Naraku : MonoBehaviour {
 		{
 			Anten.antenStart = true;
 
+			foreach (GameObject rp in restartPosition) {
+				position.Add (Vector3.Distance(playerPosition , rp.transform.position));
+				position.Sort ();
+			}
 		}
+
+
 
 
 	}
