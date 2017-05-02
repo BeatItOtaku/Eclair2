630e80b1d0cbc42c8bc13da38144173c580da11e
wararyo <wararyo@gmail.com>
2017-05-02 22:53:10 +0900

    要りそうなスクリプトを一通り作った
    もうやだ…

--- Eclair2/Assets/Scripts/Input/EclairInput.cs
+++ Eclair2/Assets/Scripts/Input/EclairInput.cs
@@ -4,24 +4,29 @@ using System;
 
 namespace wararyo.EclairInput {
 
+	/// <summary>
+	/// EclairInputをまとめる役割を果たします
+	/// </summary>
 	public static class EclairInput {
 
-		public static List<Controller> Controllers;
+		public static List<InputDevice> Controllers;
 
 		static EclairInput(){
-			Controllers = new List<Controller>();
+			Controllers = new List<InputDevice>();
 		}
 	}
 
-	public class Controller {
+	public abstract class InputDevice {
 		private string deviceName;
 		public string DeviceName{ get { return deviceName; } }
 
-		public Action OnInput(InputEvent e);
+		public Action<InputEvent> OnInput;
 
-		public Controller(string name){
+		public InputDevice(string name){
 			deviceName = name;
 		}
+
+		public abstract void Update ();
 	}
 
 	public class InputEvent {
---
+++ Eclair2/Assets/Scripts/Input/InputTypes.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace wararyo.EclairInput {
+
+/// <summary>
+/// コントローラーのA,B,X,Yなど各種ボタンとInputTypeとの紐づけ情報を記述するScriptableObject
+/// </summary>
+[CreateAssetMenu (fileName = "NewControllerKeyMap.asset", menuName = "EclairInput/ControllerKeyMap", order = 382)]
+	public class ControllerButtonMap : ScriptableObject
+	{
+		public string A;
+		public string B;
+		public string X;
+		public string Y;
+		public string R1;
+		public string L1;
+		public string R2;
+		public string L2;
+		public string R3;
+		public string L3;
+		public string START;
+		public string SELECT;
+	}
+}
