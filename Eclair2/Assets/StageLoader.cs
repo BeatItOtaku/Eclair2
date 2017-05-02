ea35e50b341a55f259bef0d9a64e0cd29f1ad96e
wararyo <wararyo@gmail.com>
2017-05-02 13:36:36 +0900

    とりあえずDebugStageがロードされるようになった

--- Eclair2/Assets/StageLoader.cs
+++ Eclair2/Assets/StageLoader.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class StageLoader : MonoBehaviour {
 
+	public string sceneName;
+
 	// Use this for initialization
 	void Start () {
-	
+		SceneManager.LoadSceneAsync (sceneName,LoadSceneMode.Additive);
 	}
 	
 	// Update is called once per frame
